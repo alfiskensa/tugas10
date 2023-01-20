8d2e99afdd3cf83d544319d8bef22caf1cebab82
alfiramdhani25 <vincentalfi@gmail.com>
2023-01-21 00:12:46 +0700

    initial commit

---
+++ application/views/cetak_nilai.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<body>
+
+<div id="container">
+<html>
+<style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        table td.tbl_header {
+            font-weight: bolder;
+            text-align: center;
+        }
+    </style>
+<h1>Cetak Nilai Mahasiswa</h1>
+
+</div>
+
+</body>
+</html>
