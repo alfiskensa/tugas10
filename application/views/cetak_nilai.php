b1501a6ef399487f4b5170c8d6944ad9ece9128c
derrysetya <derryprasetya77@gmail.com>
2023-01-21 01:15:21 +0700

    update view cetak nilai

--- application/views/cetak_nilai.php
+++ application/views/cetak_nilai.php
@@ -18,7 +18,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             text-align: center;
         }
     </style>
-<h1>Cetak Nilai Mahasiswa</h1>
+<h1>Cetak Laporan Nilai</h1>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<table>
+			<tr>
+				<td>Matakuliah</td>
+				<td>
+                <select name="kode_mk">
+                                        <option value="TA002">
+                        TA002 - Metodologi Penelitian                    </option>
+                                        <option value="TI006">
+                        TI006 - Kecerdasam Tiruan                    </option>
+                                        <option value="TI011">
+                        TI011 - Komputasi Bergerak Dan Teknologi Web                    </option>
+                                        <option value="TI016">
+                        TI016 - Kecerdasan Komputasional Dan Pembelajaran Mesin                    </option>
+                                    </select>
+            </td>
+			</tr>
+			<tr>
+			<td>Output</td>
+				<td>
+					<input type="radio" id="output1" name="output" value="pdf">
+					<label for="output1">PDF</label>
+					<input type="radio" id="output2" name="output" value="xls">
+					<label for="output2">XLS</label>
+				</td></tr>
+			<tr>
+            <td><input type="submit" name="cetak" value="Cetak"></input></td>
+        	</tr>
+		</table>
+</form>
 
 </div>
 
