85edceca3f53b3f5fa440d1be72b9eac1ed4029d
derrysetya <derryprasetya77@gmail.com>
2023-01-21 02:14:16 +0700

    add generate excel and pdf file

--- application/controllers/CetakNilai.php
+++ application/controllers/CetakNilai.php
@@ -1,5 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+// Include librari PhpSpreadsheet
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 
 class CetakNilai extends CI_Controller {
 
@@ -18,8 +22,139 @@ class CetakNilai extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+	public function __construct(){
+		parent::__construct();
+		
+		$this->load->model('nilaimodel');
+	  }
+
 	public function index()
 	{
 		$this->load->view('cetak_nilai');
+
+		
 	}
+
+	public function generateFile(){
+
+		$kode_mk = $this->input->post('kode_mk');
+		$output = $this->input->post('output');
+		
+		if($output == 'xls'){
+			$this->exportExcel();
+		}else{
+			$this->exportPDF();
+		}
+	}
+
+	public function exportPDF(){
+// panggil library yang kita buat sebelumnya yang bernama pdfgenerator
+$this->load->library('pdfgenerator');
+        
+// title dari pdf
+$this->data['title_pdf'] = 'Laporan Penjualan Toko Kita';
+
+// filename dari pdf ketika didownload
+$file_pdf = 'laporan_penjualan_toko_kita';
+// setting paper
+$paper = 'A4';
+//orientasi paper potrait / landscape
+$orientation = "portrait";
+
+$html = $this->load->view('laporan_pdf',$this->data, true);	    
+
+// run dompdf
+$this->pdfgenerator->generate($html, $file_pdf,$paper,$orientation);
+	}
+
+	public function exportExcel(){
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		// Buat sebuah variabel untuk menampung pengaturan style dari header tabel
+		$style_col = [
+		  'font' => ['bold' => true], // Set font nya jadi bold
+		  'alignment' => [
+			'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER, // Set text jadi ditengah secara horizontal (center)
+			'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
+		  ],
+		  'borders' => [
+			'top' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border top dengan garis tipis
+			'right' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN],  // Set border right dengan garis tipis
+			'bottom' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border bottom dengan garis tipis
+			'left' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN] // Set border left dengan garis tipis
+		  ]
+		];
+		// Buat sebuah variabel untuk menampung pengaturan style dari isi tabel
+		$style_row = [
+		  'alignment' => [
+			'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
+		  ],
+		  'borders' => [
+			'top' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border top dengan garis tipis
+			'right' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN],  // Set border right dengan garis tipis
+			'bottom' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border bottom dengan garis tipis
+			'left' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN] // Set border left dengan garis tipis
+		  ]
+		];
+		$sheet->setCellValue('A1', "DATA SISWA"); // Set kolom A1 dengan tulisan "DATA SISWA"
+		$sheet->mergeCells('A1:E1'); // Set Merge Cell pada kolom A1 sampai E1
+		$sheet->getStyle('A1')->getFont()->setBold(true); // Set bold kolom A1
+		// Buat header tabel nya pada baris ke 3
+		$sheet->setCellValue('A3', "NO"); // Set kolom A3 dengan tulisan "NO"
+		$sheet->setCellValue('B3', "NIM"); // Set kolom B3 dengan tulisan "NIS"
+		$sheet->setCellValue('C3', "KODE MK"); // Set kolom C3 dengan tulisan "NAMA"
+		$sheet->setCellValue('D3', "Nilai Tugas"); // Set kolom D3 dengan tulisan "JENIS KELAMIN"
+		$sheet->setCellValue('E3', "Nilai UTS"); // Set kolom E3 dengan tulisan "ALAMAT"
+		$sheet->setCellValue('F3', "Nilai UAS"); // Set kolom E3 dengan tulisan "ALAMAT"
+		// Apply style header yang telah kita buat tadi ke masing-masing kolom header
+		$sheet->getStyle('A3')->applyFromArray($style_col);
+		$sheet->getStyle('B3')->applyFromArray($style_col);
+		$sheet->getStyle('C3')->applyFromArray($style_col);
+		$sheet->getStyle('D3')->applyFromArray($style_col);
+		$sheet->getStyle('E3')->applyFromArray($style_col);
+		$sheet->getStyle('F3')->applyFromArray($style_col);
+		// Panggil function view yang ada di SiswaModel untuk menampilkan semua data siswanya
+		$siswa = $this->nilaimodel->view();
+		$no = 1; // Untuk penomoran tabel, di awal set dengan 1
+		$numrow = 4; // Set baris pertama untuk isi tabel adalah baris ke 4
+		foreach($siswa as $data){ // Lakukan looping pada variabel siswa
+		  $sheet->setCellValue('A'.$numrow, $no);
+		  $sheet->setCellValue('B'.$numrow, $data->nim);
+		  $sheet->setCellValue('C'.$numrow, $data->kode_mk);
+		  $sheet->setCellValue('D'.$numrow, $data->tugas);
+		  $sheet->setCellValue('E'.$numrow, $data->uts);
+		  $sheet->setCellValue('F'.$numrow, $data->uas);
+		  
+		  // Apply style row yang telah kita buat tadi ke masing-masing baris (isi tabel)
+		  $sheet->getStyle('A'.$numrow)->applyFromArray($style_row);
+		  $sheet->getStyle('B'.$numrow)->applyFromArray($style_row);
+		  $sheet->getStyle('C'.$numrow)->applyFromArray($style_row);
+		  $sheet->getStyle('D'.$numrow)->applyFromArray($style_row);
+		  $sheet->getStyle('E'.$numrow)->applyFromArray($style_row);
+		  $sheet->getStyle('F'.$numrow)->applyFromArray($style_row);
+		  
+		  $no++; // Tambah 1 setiap kali looping
+		  $numrow++; // Tambah 1 setiap kali looping
+		}
+		// Set width kolom
+		$sheet->getColumnDimension('A')->setWidth(5); // Set width kolom A
+		$sheet->getColumnDimension('B')->setWidth(15); // Set width kolom B
+		$sheet->getColumnDimension('C')->setWidth(25); // Set width kolom C
+		$sheet->getColumnDimension('D')->setWidth(20); // Set width kolom D
+		$sheet->getColumnDimension('E')->setWidth(30); // Set width kolom E
+		$sheet->getColumnDimension('F')->setWidth(30); // Set width kolom E
+		
+		// Set height semua kolom menjadi auto (mengikuti height isi dari kolommnya, jadi otomatis)
+		$sheet->getDefaultRowDimension()->setRowHeight(-1);
+		// Set orientasi kertas jadi LANDSCAPE
+		$sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+		// Set judul file excel nya
+		$sheet->setTitle("Laporan Nilai Mahasiswa");
+		// Proses file excel
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment; filename="Nilai Mahasiswa.xls"'); // Set nama file excel nya
+		header('Cache-Control: max-age=0');
+		$writer = new Xlsx($spreadsheet);
+		$writer->save('php://output');
+	  }
 }
--- application/views/cetak_nilai.php
+++ application/views/cetak_nilai.php
@@ -19,7 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         }
     </style>
 <h1>Cetak Laporan Nilai</h1>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="post" action="<?php echo base_url("index.php/CetakNilai/generateFile"); ?>">
 		<table>
 			<tr>
 				<td>Matakuliah</td>
